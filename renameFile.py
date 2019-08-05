8f3a7978488f5b0e30fced37208d2f1c1ae3b016
Cxx0822 <1556464090@qq.com>
2019-08-05 09:46:33 +0800

    Rename File

---
+++ renameFile.py
@@ -0,0 +1,23 @@
+import os
+
+
+def rename_files(dir_path, number):
+    """
+    批量重命名文件
+    参数：
+        dirPath：文件路径
+        number：之前图片的总和
+    """
+    file_list = os.listdir(dir_path)
+
+    index = 60
+    for item in file_list:
+        oldname = dir_path + r"\\" + file_list[index]
+        newname = dir_path + r"\\" + str(number + index) + ".jpg"
+        os.rename(oldname, newname)
+
+        index += 1
+
+if __name__ == "__main__":
+    imageDir = 'test'
+    rename_files(imageDir, 60)
